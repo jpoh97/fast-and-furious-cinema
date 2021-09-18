0eeaeec8c8937d05c44cd0df9cd609d0bd813e20
jpoh97 <juan.pablo@corp.cision.com>
2021-09-18 00:56:59 -0500

    Set up kotlin project using spring initializr

---
+++ src/main/kotlin/com/fastfurious/fastandfuriouscinema/FastAndFuriousCinemaApplication.kt
@@ -0,0 +1,11 @@
+package com.fastfurious.fastandfuriouscinema
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FastAndFuriousCinemaApplication
+
+fun main(args: Array<String>) {
+	runApplication<FastAndFuriousCinemaApplication>(*args)
+}
---
+++ src/test/kotlin/com/fastfurious/fastandfuriouscinema/FastAndFuriousCinemaApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.fastfurious.fastandfuriouscinema
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FastAndFuriousCinemaApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}

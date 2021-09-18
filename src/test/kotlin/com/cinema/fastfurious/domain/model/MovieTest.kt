942912186db1aacddcac15fcd63c82e85b6f9190
jpoh97 <juan.pablo@corp.cision.com>
2021-09-18 15:56:48 -0500

    Add test for invalid show

--- src/test/kotlin/com/cinema/fastfurious/domain/model/MovieTest.kt
+++ src/test/kotlin/com/cinema/fastfurious/domain/model/MovieTest.kt
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertNotEquals
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
 import java.time.LocalDateTime
 import java.util.Collections
 
@@ -11,8 +12,7 @@ import java.util.Collections
 class MovieTest {
 
     @Test
-    @DisplayName("2 movies with the same data must be equal")
-    fun testTwoMoviesWithEqualData() {
+    fun `2 movies with the same data must be equal`() {
         val currentTime = LocalDateTime.now().plusDays(1)
         val shows = Collections.singletonList(Show(time = currentTime, price = 20.0))
         val reviews = Collections.singletonList(ReviewRating(5f))
@@ -23,10 +23,9 @@ class MovieTest {
     }
 
     @Test
-    @DisplayName("2 movies with the different data must be not equal")
-    fun testTwoMoviesWithNotEqualData() {
-        val currentTime = LocalDateTime.now().plusDays(1)
-        val shows = Collections.singletonList(Show(time = currentTime, price = 20.0))
+    fun `2 movies with the different data must be not equal`() {
+        val tomorrowTime = LocalDateTime.now().plusDays(1)
+        val shows = Collections.singletonList(Show(time = tomorrowTime, price = 20.0))
         val reviews = Collections.singletonList(ReviewRating(5f))
         val movie1 = Movie(id = 1, shows, reviews)
         val movie2 = Movie(id = 2, shows, reviews)
@@ -34,4 +33,13 @@ class MovieTest {
         assertNotEquals(movie1, movie2)
     }
 
+    @Test
+    fun `showtime in the past should throws IllegalArgumentException`() {
+        val yesterdayTime = LocalDateTime.now().minusDays(1)
+        val invalidShowtime = assertThrows<IllegalArgumentException> {
+            Show(time = yesterdayTime, price = 20.0)
+        }
+
+        assertEquals("The showtime can not be in the past", invalidShowtime.message)
+    }
 }

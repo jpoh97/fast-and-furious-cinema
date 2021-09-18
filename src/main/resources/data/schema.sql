46621faf8356ece6f21fb6e174b8197fc1c29dce
jpoh97 <juan.pablo@corp.cision.com>
2021-09-18 16:28:29 -0500

    Added show and review rating tables

--- src/main/resources/data/schema.sql
+++ src/main/resources/data/schema.sql
@@ -2,3 +2,14 @@ create table if not exists movie (
   id        int auto_increment primary key,
   imdb_id   varchar(32) not null
 );
+
+create table if not exists show (
+  movie_id  int not null,
+  time      timestamp not null,
+  price     decimal(20, 2) not null
+);
+
+create table if not exists review_rating (
+  movie_id  int not null,
+  value     decimal(3, 2) not null
+);

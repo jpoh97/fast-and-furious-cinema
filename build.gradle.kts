49ccea3d4f882e13abef885494d00629ab7ecf47
jpoh97 <juan.pablo@corp.cision.com>
2021-09-22 20:59:56 -0500

    Added mocking dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -29,6 +29,7 @@ dependencies {
 	runtimeOnly("org.springframework.boot:spring-boot-devtools")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("io.projectreactor:reactor-test")
+	testImplementation("io.mockk:mockk:1.12.0")
 }
 
 tasks.withType<KotlinCompile> {

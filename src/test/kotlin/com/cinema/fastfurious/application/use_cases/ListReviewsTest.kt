177d2bc178077ad9f2dc070faf3839994b881acc
jpoh97 <juan.pablo@corp.cision.com>
2021-09-22 21:56:45 -0500

    Added test cases to list review by movie

---
+++ src/test/kotlin/com/cinema/fastfurious/application/use_cases/ListReviewsTest.kt
@@ -0,0 +1,41 @@
+package com.cinema.fastfurious.application.use_cases
+
+import com.cinema.fastfurious.domain.MovieRepository
+import com.cinema.fastfurious.domain.model.ReviewRating
+import io.mockk.every
+import io.mockk.mockk
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+import reactor.core.publisher.Flux
+import reactor.test.StepVerifier
+import java.util.stream.Stream
+
+@DisplayName("List movie review use case tests")
+class ListReviewsTest {
+
+    @Test
+    fun `list movie reviews successfully`() {
+        val movieRepository = mockk<MovieRepository>()
+        val listReviewsByMovie = ListReviews(movieRepository)
+        val reviewRating1 = ReviewRating(id = 1, movieId = 42, value = 3f)
+
+        every { movieRepository.listReviewsByMovie(movieId = 42) } returns Flux.fromStream(Stream.of(reviewRating1))
+
+        val ratedMovie = listReviewsByMovie.execute(movieId = 42)
+
+        StepVerifier.create(ratedMovie).expectNext(reviewRating1).verifyComplete()
+    }
+
+
+    @Test
+    fun `list movie reviews empty list successfully`() {
+        val movieRepository = mockk<MovieRepository>()
+        val listReviewsByMovie = ListReviews(movieRepository)
+
+        every { movieRepository.listReviewsByMovie(movieId = 42) } returns Flux.fromStream(Stream.empty())
+
+        val ratedMovie = listReviewsByMovie.execute(movieId = 42)
+
+        StepVerifier.create(ratedMovie).verifyComplete()
+    }
+}
